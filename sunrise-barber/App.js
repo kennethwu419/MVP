9f999f4d9e7b71c127d9dbf982efb02749a3b902
Kenneth Wu <kunhan419.wu@gmail.com>
2022-03-15 14:19:19 +0800

    iOS simulator rendering app

--- sunrise-barber/App.js
+++ sunrise-barber/App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Modal } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={require('./assets/logo.png')}
+       style={{width: 150, height: 150}} />
       <Text style={styles.baseText}>SUNRISE BARBER© 2022</Text>
+      <Text style={styles.lowerText}>TAIPEI, TAIWAN</Text>
+      <hr/>
+      <form>
+        <input type='text' value='' placeholder='email'/>
+      </form>
+      <hr/>
+      <form>
+        <input type='text' value=''placeholder='password'/>
+      </form>
+      <hr/>
+      <form>
+        <input type='submit' value='login'/>
+      </form>
+      <TouchableOpacity>
+        <Text style={{ color: 'blue', fontSize: 10 }}>
+          sign-up
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +42,9 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
+  lowerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 10,
+  }
 });
---
+++ sunrisebarber/App.js
@@ -0,0 +1,157 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/logo.png')}
+       style={{width: 150, height: 150}} />
+      <Text style={styles.baseText}>SUNRISE BARBER© 2022</Text>
+      <Text style={styles.lowerText}>TAIPEI, TAIWAN</Text>
+      <TextInput style={styles.input} value='' placeholder='email'/>
+      <TextInput style={styles.input} value='' placeholder='password'/>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E95515',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  baseText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  lowerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 10,
+    marginBottom: 5
+  },
+  input: {
+    height: 40,
+    margin: 5,
+    width: 180,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white'
+  },
+});
+
+export default App;
+
+
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.js</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });

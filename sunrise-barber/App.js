c493ade079d15c6c56be97c917a0c52ae2bc02b1
Kenneth Wu <kunhan419.wu@gmail.com>
2022-03-15 09:15:14 +0800

    setting up login page

--- sunrise-barber/App.js
+++ sunrise-barber/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.baseText}>SUNRISE BARBER 2022</Text>
+      <Text style={styles.baseText}>SUNRISE BARBER© 2022</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E95515',
-    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },

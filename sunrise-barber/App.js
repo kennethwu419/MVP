0911f5249ae7eb996ecaf024756fc80d830f00c2
Kenneth Wu <kunhan419.wu@gmail.com>
2022-03-15 09:03:30 +0800

    updated title screen

--- sunrise-barber/App.js
+++ sunrise-barber/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.baseText}>SUNRISE BARBER 2022</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E95515',
+    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  baseText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
